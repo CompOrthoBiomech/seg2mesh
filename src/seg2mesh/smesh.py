0020119f4700252627e1a76bb759bde10c78bd66
Scott Sibole <scott.sibole@gmail.com>
2026-08-03 16:10:22 -0700

    Added mesh repair with pymeshfix, adaptive remeshing with mmg3d, and made general improvements

--- src/seg2mesh/smesh.py
+++ src/seg2mesh/smesh.py
@@ -2,16 +2,20 @@ from typing import Literal
 
 import numpy as np
 import numpy.typing as npt
+import pymeshfix as mf
 from loguru import logger
+from mmgpy.metrics import create_isotropic_metric
 from pyacvd import Clustering
 from pyvista import PolyData
 from sklearn.metrics import accuracy_score, f1_score, jaccard_score
 from vtkmodules.all import (
     VTK_CHAR,
     vtkCleanPolyData,
+    vtkCurvatures,
     vtkDataObject,
     vtkDataSetAttributes,
     vtkDiscreteFlyingEdges3D,
+    vtkFeatureEdges,
     vtkFillHolesFilter,
     vtkFloatArray,
     vtkGeometryFilter,
@@ -21,8 +25,10 @@ from vtkmodules.all import (
     vtkImageStencil,
     vtkImplicitPolyDataDistance,
     vtkPolyData,
+    vtkPolyDataConnectivityFilter,
     vtkPolyDataNormals,
     vtkPolyDataToImageStencil,
+    vtkSmoothPolyDataFilter,
     vtkThreshold,
     vtkUnstructuredGrid,
     vtkWindowedSincPolyDataFilter,
@@ -111,6 +117,14 @@ def voxelize_mesh(
     return stencil.GetOutput()
 
 
+def remove_islands(poly: vtkPolyData) -> vtkPolyData:
+    connected = vtkPolyDataConnectivityFilter()
+    connected.SetInputData(poly)
+    connected.SetExtractionModeToLargestRegion()
+    connected.Update()
+    return connected.GetOutput()
+
+
 def image_boolean(image1: vtkImageData, image2: vtkImageData, operation: Literal["Union", "Intersection", "Difference"]) -> vtkImageData:
     boolean = vtkImageMathematics()
     boolean.SetInput1Data(image1)
@@ -152,15 +166,16 @@ def evaluate_volume_metrics(poly1: vtkPolyData, poly2: vtkPolyData, voxel_edge: 
 
 def evaluate_distance_metrics(poly1: vtkPolyData, poly2: vtkPolyData) -> tuple[vtkPolyData, dict[str, float]]:
     """
-    Calculates distance error metrics including Hausdorff distance, Mean Symmetric Surface Distance, and Root Mean Square Distance.
+    Calculates distance error metrics including Hausdorff distance, Mean Symmetric Surface Distance, and
+    Root Mean Square Distance.
 
     :param poly1: The current mesh to compare.
     :param poly2: The reference mesh to compare against.
 
-    :returns: A tuple containing PolyData with poly1 topology with shortest (point-cell calculated) distances from poly1 to poly2 stored on
-    vertices and metrics stored as FieldData and a dictionary of metrics. NOTE: The vertex distances are only poly1 to poly2 distances, but metrics
-    are calculated using both poly1 to poly2 and poly2 to poly1 distances.
-
+    :returns: A tuple containing PolyData with poly1 topology with shortest (point-cell calculated) distances from
+        poly1 to poly2 stored on vertices and metrics stored as FieldData and a dictionary of metrics. NOTE: The
+        vertex distances are only poly1 to poly2 distances, but metrics are calculated using both poly1 to poly2
+        and poly2 to poly1 distances.
     """
     logger.info("Evaluating Distance Metrics")
     distance = vtkHausdorffDistancePointSetFilter()
@@ -172,48 +187,62 @@ def evaluate_distance_metrics(poly1: vtkPolyData, poly2: vtkPolyData) -> tuple[v
     distance1 = distance.GetOutput(0)
     distance2 = distance.GetOutput(1)
 
-    total_points = distance2.GetNumberOfPoints() + distance2.GetNumberOfPoints()
+    total_points = distance1.GetNumberOfPoints() + distance2.GetNumberOfPoints()
     distance1_array = vtk_to_numpy(distance1.GetPointData().GetArray("Distance"))
     distance2_array = vtk_to_numpy(distance2.GetPointData().GetArray("Distance"))
     mssd = (np.sum(distance1_array) + np.sum(distance2_array)) / total_points
     mssd_array = vtkFloatArray()
     mssd_array.InsertNextValue(mssd)
     mssd_array.SetName("Mean Symmetric Surface Distance")
-    distance2.GetFieldData().AddArray(mssd_array)
+    distance1.GetFieldData().AddArray(mssd_array)
     rmse = np.sqrt((np.sum(distance1_array**2) + np.sum(distance2_array**2)) / total_points)
     rmse_array = vtkFloatArray()
     rmse_array.InsertNextValue(rmse)
     rmse_array.SetName("Root Mean Square Distance")
-    distance2.GetFieldData().AddArray(rmse_array)
+    distance1.GetFieldData().AddArray(rmse_array)
 
     metrics = {
-        "Hausdorff Distance": distance2.GetFieldData().GetArray("HausdorffDistance").GetValue(0),
+        "Hausdorff Distance": distance1.GetFieldData().GetArray("HausdorffDistance").GetValue(0),
         "Mean Symmetric Surface Distance": mssd,
         "Root Mean Square Distance": rmse,
     }
     for k, v in metrics.items():
         logger.info(f"...{k}: {v}")
 
-    return distance2, metrics
+    return distance1, metrics
 
 
-def taubin_smooth(poly: vtkPolyData, iterations: int = 40, passband: float = 0.01) -> vtkPolyData:
+def taubin_smooth(poly: vtkPolyData, iterations: int = 40, smoothing_factor: float = 0.8) -> vtkPolyData:
     """
     Apply Taubin smoothing to the input vtkPolyData.
 
     :param poly: The input vtkPolyData to be smoothed.
     :param iterations: The number of smoothing iterations. More iterations result in smoother output.
-    :param passband: The passband parameter for the smoothing filter. Lower values result in smoother output.
+    :param smoothing_factor: The adjusts the passband parameter for the smoothing filter to mapping 0.0 to 1.0 to a passband of 0.0001 to 1.0. This is equivalent to the Slicer3D implementation.
     :return: The smoothed vtkPolyData.
     """
+    passband = np.pow(10.0, -4.0 * smoothing_factor)
     smooth = vtkWindowedSincPolyDataFilter()
     smooth.SetInputData(poly)
     smooth.SetNumberOfIterations(iterations)
     smooth.SetPassBand(passband)
     smooth.BoundarySmoothingOff()
     smooth.FeatureEdgeSmoothingOff()
-    smooth.NonManifoldSmoothingOn()
+    smooth.SetFeatureAngle(180.0)
+    smooth.NonManifoldSmoothingOff()
     smooth.NormalizeCoordinatesOn()
+    smooth.Update()
+    return smooth.GetOutput()
+
+
+def constrained_laplacian_smooth(
+    poly: vtkPolyData, constrain_poly: vtkPolyData, iterations: int = 40, relaxation_factor: float = 1.0
+) -> vtkPolyData:
+    smooth = vtkSmoothPolyDataFilter()
+    smooth.SetInputData(poly)
+    smooth.SetSourceData(constrain_poly)
+    smooth.SetNumberOfIterations(iterations)
+    smooth.SetConvergence(0.0)
     smooth.Update()
     return smooth.GetOutput()
 
@@ -225,6 +254,19 @@ def _grid_to_poly(grid: vtkUnstructuredGrid) -> vtkPolyData:
     return geo.GetOutput()
 
 
+def repair_mesh(mesh: vtkPolyData) -> vtkPolyData:
+    """
+    Uses the pymeshfix library to clean and repair the input mesh
+
+    :return: The repaired vtkPolyData.
+    """
+    pv_poly = PolyData(mesh)
+    meshfix = mf.MeshFix(pv_poly)
+    meshfix.clean()
+    meshfix.repair()
+    return clean_poly(meshfix.mesh)
+
+
 def clean_poly(poly: vtkPolyData) -> vtkPolyData:
     """
     Clean the input vtkPolyData using vtkCleanPolyData and fill any holes using vtkFillHolesFilter.
@@ -232,6 +274,17 @@ def clean_poly(poly: vtkPolyData) -> vtkPolyData:
     :param poly: The input vtkPolyData to be cleaned.
     :return: The cleaned vtkPolyData with holes filled.
     """
+
+    edges = vtkFeatureEdges()
+    edges.SetInputData(poly)
+    edges.BoundaryEdgesOn()
+    edges.NonManifoldEdgesOn()
+    edges.FeatureEdgesOff()
+    edges.Update()
+    bad_edges = edges.GetOutput().GetNumberOfCells()
+    if bad_edges > 0:
+        logger.warning(f"Found {bad_edges} bad edges")
+
     clean = vtkCleanPolyData()
     clean.SetInputData(poly)
     clean.Update()
@@ -247,6 +300,53 @@ def clean_poly(poly: vtkPolyData) -> vtkPolyData:
     return fillholes.GetOutput()
 
 
+def get_curvatures(poly: vtkPolyData, curvature_type: Literal["maximum", "minimum", "gaussian", "mean"] = "maximum") -> vtkPolyData:
+    """
+    Calculate the curvatures of the given vtkPolyData.
+
+    :param poly: The vtkPolyData to calculate curvatures for.
+    :param curvature_type: The type of curvature to calculate.
+
+    :return: The vtkPolyData with curvature values added as point data.
+    """
+    ctype = {"gaussian": 0, "mean": 1, "maximum": 2, "minimum": 3}
+    curvature = vtkCurvatures()
+    curvature.SetCurvatureType(ctype[curvature_type])
+    curvature.SetInputData(poly)
+    curvature.Update()
+    return curvature.GetOutput()
+
+
+def mmg_remesh(
+    poly: vtkPolyData, hmax: float = 1.0, hmin: float = 0.2, divisions_per_circle: float = 16, hgrad: float = 1.5
+) -> vtkPolyData:
+    """
+    Adaptive remeshing using mmg3d. Note: we override the default Hausdorff parameter and rely
+    on metric values defined by local curvature to control element size.
+
+    :param poly: The vtkPolyData to be remeshed.
+    :param hmax: The maximum target element edge length.
+    :param hmin: The minimum target element edge length.
+    :param divisions_per_circle: Local target edge length defined as a function of radius of curvature. Number of divisions per circle of this radius.
+    :param hgrad: Ratio defining how fast element edge length can change. Higher values mean faster changes.
+
+    :return: The remeshed vtkPolyData.
+    """
+    pv_poly = PolyData(repair_mesh(poly))
+    min_curvature = pv_poly.curvature(curv_type="minimum")
+    max_curvature = pv_poly.curvature(curv_type="maximum")
+    curvatures = np.maximum(np.abs(min_curvature), np.abs(max_curvature))
+    curvatures[curvatures < 1e-7] = 1e-7
+    h = (2 * np.pi / divisions_per_circle) / curvatures
+    h = np.clip(h, a_min=hmin, a_max=hmax)
+    h = create_isotropic_metric(h)
+
+    pv_poly.point_data["metric"] = h
+    pv_poly = pv_poly.mmg.remesh(hausd=100.0, hgrad=hgrad)
+    pv_poly = pv_poly.mmg.remesh_optimize()
+    return repair_mesh(clean_poly(pv_poly))
+
+
 def remesh(poly: vtkPolyData, edge_length: float = 1.0) -> vtkPolyData:
     """
     Remesh the input vtkPolyData using ACVD (Approximated Centroidal Voronoi Diagrams).
@@ -256,7 +356,7 @@ def remesh(poly: vtkPolyData, edge_length: float = 1.0) -> vtkPolyData:
     :return: The remeshed vtkPolyData.
     """
     logger.info("...Remeshing polydata using ACVD")
-    pv_poly = PolyData(poly)
+    pv_poly = PolyData(repair_mesh(poly))
     # get average cell edge length
     edges = pv_poly.extract_feature_edges(boundary_edges=False, feature_edges=False, manifold_edges=True, non_manifold_edges=False)
     edges = edges.compute_cell_sizes(length=True, area=False, volume=False)
@@ -268,7 +368,7 @@ def remesh(poly: vtkPolyData, edge_length: float = 1.0) -> vtkPolyData:
     # Perform ACVD remesh and clean the output
     cluster = Clustering(pv_poly)
     cluster.cluster(num_clusters)
-    mesh = clean_poly(cluster.create_mesh())
+    mesh = repair_mesh(cluster.create_mesh())
     return mesh
 
 
@@ -300,6 +400,7 @@ def extract_isocontours(volume: NamedVTKImage) -> dict[str, vtkPolyData]:
     iso = vtkDiscreteFlyingEdges3D()
     iso.SetInputData(volume.image)
     iso.GenerateValues(len(volume.lut), 1, len(volume.lut))
+    iso.ComputeNormalsOff()
     iso.Update()
     all_contours = iso.GetOutput()
 
@@ -314,5 +415,5 @@ def extract_isocontours(volume: NamedVTKImage) -> dict[str, vtkPolyData]:
         threshold.SetUpperThreshold(label_id)
         threshold.Update()
 
-        contours[name] = clean_poly(_grid_to_poly(threshold.GetOutput()))
+        contours[name] = remove_islands(_grid_to_poly(threshold.GetOutput()))
     return contours
